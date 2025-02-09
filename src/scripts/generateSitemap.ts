7c9f885a0310c1abda93ed9a53de71da5f7091ae
ericbreitnicholson <ericbreitnicholson@gmail.com>
2025-02-09 12:49:20 -0500

    [Cursor] Fix sitemap.xml schema location format

--- src/scripts/generateSitemap.ts
+++ src/scripts/generateSitemap.ts
@@ -30,7 +30,7 @@ async function generateSitemap() {
         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
         xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
         xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-        http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+                           http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
 `
 
   // Add homepage
